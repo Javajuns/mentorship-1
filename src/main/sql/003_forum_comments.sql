b06f1248ce0039ac652eb754ddcc92ee0aaa8052
Ivan Kovalenko <iv.nik.kov@gmail.com>
2015-02-23 23:13:31 +0200

    foreigen key and index table forum_comments

--- src/main/sql/003_forum_comments.sql
+++ src/main/sql/003_forum_comments.sql
@@ -13,3 +13,7 @@ REFERENCES PUBLIC.users(id);
 ALTER TABLE forum_comments ADD CONSTRAINT fk_topic_id
 FOREIGN KEY (topic_id)
 REFERENCES PUBLIC.forum_topics(id);
+
+CREATE INDEX idx_user_id ON forum_comments(user_id);
+
+CREATE INDEX idx_topic_id ON forum_comments(topic_id);
