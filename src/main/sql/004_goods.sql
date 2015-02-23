ee4e683562a61a1983e4c71f653c6ecc31a5fa74
Ivan Kovalenko <iv.nik.kov@gmail.com>
2015-02-23 23:04:53 +0200

    foreigen key and index table goods

--- src/main/sql/004_goods.sql
+++ src/main/sql/004_goods.sql
@@ -5,3 +5,11 @@ price DOUBLE,
 category_id INT,
 rest DOUBLE
 );
+
+ALTER TABLE goods ADD CONSTRAINT fk_category_id
+FOREIGN KEY (category_id)
+REFERENCES PUBLIC.CATEGORY(id);
+
+CREATE INDEX idx_name ON goods(name);
+
+
