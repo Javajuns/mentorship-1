f2c2878a4665b0cc16d8bbd6a358522df7b931b5
prof <tnd_prof@yahoo.com>
2015-02-24 18:49:21 +0200

    Spring Boot.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,31 @@
 package hello;
 
+import com.github.javamentorship.mentorship.DBImpl;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping(value = "/")
+    public String index(Model model) {
+        rs = DBImpl.select()
+
+        List<Map<String, Object>> categories =[[id:
+        1, name:'phones', parentid:1]];
+        model.addAttribute("categories", categories);
+        return "/hello/index.jsp";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Integer id, String name, Integer parentId) {
+        //TODO save to db
+        return "redirect:/";
     }
 
 }
