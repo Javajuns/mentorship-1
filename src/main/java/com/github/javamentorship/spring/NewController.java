80228cf09b219256439491dc9861022949bbbcfd
prof <tnd_prof@yahoo.com>
2015-02-25 21:10:19 +0200

    Spring Boot.

--- src/main/java/com/github/javamentorship/spring/NewController.java
+++ src/main/java/com/github/javamentorship/spring/NewController.java
@@ -1,25 +1,25 @@
 package com.github.javamentorship.spring;
 
-import com.github.javamentorship.mentorship.DBImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class NewController {
 
-    @RequestMapping({"/", "/hello"})
-    public String viewTable(Map<String, Object> model) throws SQLException, ClassNotFoundException {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NewController.class);
+
+    @RequestMapping("/")
+    public ModelAndView viewTable(Map<String, Object> model) throws SQLException, ClassNotFoundException {
         ResultSet rs = DBImpl.select("SELECT * FROM category");
         List<Map<String, Object>> categories = new ArrayList<Map<String, Object>>();
-
         while (rs.next()) {
             Map<String, Object> map = new HashMap<String, Object>();
             map.put("id", String.valueOf(rs.getInt("id")));
@@ -28,15 +28,8 @@ public class NewController {
             categories.add(map);
         }
         model.put("categories", categories);
-        return "index";
-    }
-
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(Integer id, String name, Integer parentId) throws SQLException, ClassNotFoundException {
-        //code for insert
-        //"INSERT INTO category (id,name,parent_id) VALUES (null,'" + name + "','" + parentId + "')"
-        int result = DBImpl.update("UPDATE category SET NAME='" + name + "', PARENT_ID='" + parentId + "' WHERE ID=" + id);
-        return "redirect:/";
+        LOGGER.debug("Received request for create index view");
+        return new ModelAndView("index", "nop", categories);
     }
 
 }
