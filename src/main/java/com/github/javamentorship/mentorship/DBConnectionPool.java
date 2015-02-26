2d8e56de5782ed62761390444e353074090eb8e6
profeg <tnd_prof@yahoo.com>
2015-02-26 17:18:24 +0200

    Spring Ok. Database Ok. Change in CategoryDao.

--- src/main/java/com/github/javamentorship/mentorship/DBConnectionPool.java
+++ src/main/java/com/github/javamentorship/mentorship/DBConnectionPool.java
@@ -3,13 +3,10 @@ package com.github.javamentorship.mentorship;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-/**
- * Created by ivan on 24.02.2015.
- */
 public class DBConnectionPool {
 
     public static final String HSQLDB = "org.hsqldb.jdbcDriver";
-    public static final String URL = "jdbc:hsqldb:file:target/devDb/devDb";
+    public static final String URL = "jdbc:hsqldb:file:target/devDb/devDb;ifexist=true";
     public static final String USER = "sa";
     public static final String PASS = "";
     private static Connection conn;
