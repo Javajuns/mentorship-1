debf9b5257b79118bf6ee6deb568a1f273ca3cd8
profeg <tnd_prof@yahoo.com>
2015-02-26 03:22:51 +0200

    This is some temporally commit. Nothing special. Spring Boot is alive now Try to plug CategoryDao, and understand what the f#@#@ with this Fu#@#  db which called hsqldb  =)

--- src/main/java/com/github/javamentorship/spring/NewController.java
+++ src/main/java/com/github/javamentorship/spring/NewController.java
@@ -1,15 +1,14 @@
 package com.github.javamentorship.spring;
 
+import com.github.javamentorship.mentorship.CategoryDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.Map;
 
 @RestController
 public class NewController {
@@ -18,18 +17,8 @@ public class NewController {
 
     @RequestMapping("/")
     public ModelAndView viewTable(Map<String, Object> model) throws SQLException, ClassNotFoundException {
-        ResultSet rs = DBImpl.select("SELECT * FROM category");
-        List<Map<String, Object>> categories = new ArrayList<Map<String, Object>>();
-        while (rs.next()) {
-            Map<String, Object> map = new HashMap<String, Object>();
-            map.put("id", String.valueOf(rs.getInt("id")));
-            map.put("name", rs.getString("name"));
-            map.put("parent_id", String.valueOf(rs.getInt("parent_id")));
-            categories.add(map);
-        }
-        model.put("categories", categories);
         LOGGER.debug("Received request for create index view");
-        return new ModelAndView("index", "nop", categories);
+        return new ModelAndView("index", "nop", new CategoryDao().listAll());
     }
 
 }
