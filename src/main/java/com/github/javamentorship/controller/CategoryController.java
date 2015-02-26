c0d3716479dc760050b9beca8d796ccb099169f1
Evgen Serdyuk <all.nothing.name@gmail.com>
2015-02-27 00:49:21 +0200

    refactoring

--- src/main/java/com/github/javamentorship/controller/CategoryController.java
+++ src/main/java/com/github/javamentorship/controller/CategoryController.java
@@ -11,7 +11,6 @@ import java.sql.SQLException;
 
 public class CategoryController extends HttpServlet {
 
-    private HttpServletRequest request;
     private CategoryDao categoryDao;
 
     @Override
@@ -22,21 +21,20 @@ public class CategoryController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        request = req;
 
         Integer id = Integer.valueOf(req.getParameter("id"));
         String name = req.getParameter("name");
         Integer parentId = Integer.valueOf(req.getParameter("parent_id"));
 
         try {
-            if (checkParameter("Remove")){
+            if (checkParameter("Remove", req)){
 
             }
-            if (checkParameter("Update")){
+            if (checkParameter("Update", req)){
                 categoryDao.update(name, parentId, id);
             }
 
-            if (checkParameter("Insert")){
+            if (checkParameter("Insert", req)){
                 categoryDao.insert(name);
             }
         } catch (SQLException e) {
@@ -49,8 +47,8 @@ public class CategoryController extends HttpServlet {
         resp.setHeader("Location", "/mentorship/tableCategoryView.jsp");
     }
 
-    private boolean checkParameter(String param) {
-        return param.equals(request.getParameter(param));
+    private boolean checkParameter(String param, HttpServletRequest request1) {
+        return param.equals(request1.getParameter(param));
     }
 
 }
