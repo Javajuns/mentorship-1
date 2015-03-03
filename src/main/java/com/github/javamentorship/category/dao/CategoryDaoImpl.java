c25b6967040ef55b67a23540f861929458cbcd67
Sergey Ponomarev <stokito@gmail.com>
2015-03-03 17:02:04 +0200

    Category DAO rewritted to JPA

--- src/main/java/com/github/javamentorship/category/dao/CategoryDaoImpl.java
+++ src/main/java/com/github/javamentorship/category/dao/CategoryDaoImpl.java
@@ -1,63 +1,52 @@
 package com.github.javamentorship.category.dao;
 
 import com.github.javamentorship.category.domain.Category;
-import com.github.javamentorship.category.hibernate.HibernateUtils;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.sql.SQLException;
 import java.util.List;
 
 
 @Component
+@Repository
 public class CategoryDaoImpl implements CategoryDao {
 
-    public SessionFactory sessionFactory;
+    @PersistenceContext
+    public EntityManager entityManager;
 
     public CategoryDaoImpl() {
     }
 
+    @Transactional
     @Override
     public synchronized void deleteCategory(Category category) throws SQLException {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.delete(category);
-        session.getTransaction().commit();
+        entityManager.remove(category);
     }
 
     @Override
     public synchronized Category getById(Integer id) throws SQLException {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        Category result =  (Category) session.get(Category.class, id);
-        session.getTransaction().commit();
-        return result;
+        return entityManager.find(Category.class, id);
     }
 
+    @Transactional
     @Override
     public synchronized void update(Category category) throws SQLException {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.update(category);
-        session.getTransaction().commit();
+        entityManager.persist(category);
     }
 
+    @Transactional
     @Override
     public synchronized void addCategory(Category category) throws SQLException {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        session.save(category);
-        session.getTransaction().commit();
+        entityManager.persist(category);
     }
 
     @Override
     public synchronized List<Category> listCategory() throws SQLException {
-        Session session = sessionFactory.getCurrentSession();
-        session.beginTransaction();
-        List<Category> categories = session.createQuery("from Category").list();
-        session.getTransaction().commit();
+        List<Category> categories = entityManager.createQuery("from Category", Category.class).getResultList();
         return categories;
     }
-
 }
