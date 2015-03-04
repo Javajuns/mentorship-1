0093875fb99f2687f773e53d6f425cb52b33a40c
kaidzen <karpov.srg@gmail.com>
2015-03-04 21:15:40 +0200

    implement Hibernet-JPA on UsersDaoImpl

--- src/main/java/com/github/javamentorship/category/dao/UsersDaoImpl.java
+++ src/main/java/com/github/javamentorship/category/dao/UsersDaoImpl.java
@@ -1,59 +1,55 @@
 package com.github.javamentorship.category.dao;
 
+import com.github.javamentorship.category.domain.Category;
 import com.github.javamentorship.category.domain.Users;
 import com.github.javamentorship.category.hibernate.HibernateUtils;
 import org.hibernate.Session;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 import java.util.List;
 
 
-@Component
+@Repository
 public class UsersDaoImpl implements UsersDao<Users> {
+
+    @PersistenceContext
+    public EntityManager entityManager;
 
     public UsersDaoImpl() {
     }
 
+    @Transactional
     @Override
-    public synchronized void delete(Users del_obj) {
-        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        session.delete(del_obj);
-        session.getTransaction().commit();
+    public synchronized void delete(Users user) {
+        entityManager.remove(user);
     }
 
     @Override
     public synchronized Users getById(Integer id) {
-        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Users result = (Users) session.get(Users.class, id);
-        session.getTransaction().commit();
-        return result;
+        return entityManager.find(Users.class, id);
     }
 
+    @Transactional
     @Override
     public synchronized void update(Users user) {
-        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        session.update(user);
-        session.getTransaction().commit();
+        entityManager.persist(user);
     }
 
+    @Transactional
     @Override
     public synchronized void add(Users user) {
-        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        session.save(user);
-        session.getTransaction().commit();
+        entityManager.persist(user);
     }
 
     @Override
     public synchronized List<Users> list() {
-        Session session = HibernateUtils.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        List<Users> categories = session.createQuery("from Users").list();
-        session.getTransaction().commit();
-        return categories;
+        List<Users> users = entityManager.createQuery("from Users", Users.class).getResultList();
+        return users;
+
     }
 
 }
