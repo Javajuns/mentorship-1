e0fc9bd3dd4fae94538eb66ae4461bb221edc822
Oleksiy Ostanin <tnd.prof@gmail.com>
2015-03-05 19:09:56 +0200

    Add some data into db.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,12 @@
 INSERT INTO category (name, parent_id) VALUES ('Phones', null);
+INSERT INTO category (name, parent_id) VALUES ('Computers', NULL);
+INSERT INTO category (name, parent_id) VALUES ('Cars', NULL);
 INSERT INTO users (login) VALUES ('ivan');
+INSERT INTO users (login, first_name, second_name, email, date_created, is_admin)
+VALUES ('prof', 'oleksiy', 'ostanin', 'tnd.prof@gmail.com', '2015-03-06', 1);
+INSERT INTO goods (NAME) VALUES ('Lenovo t520');
+INSERT INTO goods (NAME) VALUES ('Asus K53SM');
+INSERT INTO goods (NAME) VALUES ('Samsung galaxy s4');
+INSERT INTO goods (NAME) VALUES ('Samsung galaxy s3');
+INSERT INTO goods (NAME) VALUES ('Audi S4');
+INSERT INTO goods (NAME) VALUES ('Porshe Carrera 911');
