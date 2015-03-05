de813efe45cf778fb2a71b708a1c95660764fd53
Oleksiy Ostanin <tnd.prof@gmail.com>
2015-03-05 20:24:15 +0200

    Some fix in UsersController.java

--- src/main/java/com/github/javamentorship/category/controller/UsersController.java
+++ src/main/java/com/github/javamentorship/category/controller/UsersController.java
@@ -1,8 +1,7 @@
-package com.github.javamentorship.category.controller;
+package com.github.javamentorship.user.controller;
 
 import com.github.javamentorship.category.command.UsersInsertForm;
 import com.github.javamentorship.category.command.UsersUpdateForm;
-import com.github.javamentorship.category.dao.RepositoryDao;
 import com.github.javamentorship.category.dao.UsersDao;
 import com.github.javamentorship.category.domain.User;
 import org.slf4j.Logger;
@@ -13,9 +12,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.util.List;
 
 @Controller
 @RequestMapping(value = "/users")
@@ -29,7 +26,7 @@ public class UsersController {
 
     @RequestMapping("")
     public ModelAndView index() {
-        LOGGER.debug("Received request for SELECT from table USERS");
+        LOGGER.debug("Received request for SELECT from table CATEGORY");
         ModelAndView modelAndView = new ModelAndView("users");
         Iterable<User> categories = usersDao.findAll();
         modelAndView.addObject("viewUsers", categories);
@@ -39,14 +36,8 @@ public class UsersController {
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public ModelAndView add() {
         LOGGER.debug("Received request for get InsertUsers View");
-        ModelAndView modelAndView = new ModelAndView("users_insert");
+        ModelAndView modelAndView = new ModelAndView("user_insert");
         modelAndView.addObject("insert_form", new UsersInsertForm());
-        /*List<Users> parentCategories = usersDao.list();
-        Map<String, String> parentUsersItems = new LinkedHashMap<String, String>();
-        for (Users users : parentCategories) {
-            parentUsersItems.put(users.getId().toString(), users.getName());
-        }
-        modelAndView.addObject("parentCategories", parentUsersItems);*/
         return modelAndView;
     }
 
@@ -54,22 +45,29 @@ public class UsersController {
     public String save(@Valid @ModelAttribute("insert_form") UsersInsertForm form, BindingResult result) {
         LOGGER.debug("Received request to create {}", form);
         User user = new User();
-     /*   users.setName(form.getName());
-        users.setParentId(form.getParentId());*/
+        user.setLogin(form.getLogin());
+        user.setFirstName(form.getFirstName());
+        user.setSecondName(form.getSecondName());
+        user.setEmail(form.getEmail());
+        user.setDateCreated(form.getDate_created());
+        user.setIsAdmin(form.getIsAdmin());
         usersDao.save(user);
         return REDIRECT_TO_INDEX;
     }
 
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
     public ModelAndView edit(@PathVariable("id") int id) {
+        ModelAndView modelAndView = new ModelAndView("user_update");
         UsersUpdateForm updateForm = new UsersUpdateForm();
-        User user = (User) usersDao.findOne(id);
-/*
-        updateForm.setId(users.getId());
-        updateForm.setName(users.getName());
-        updateForm.setParentId(users.getParentId());
-*/
-        return new ModelAndView("users_update", "update_form", updateForm);
+        User user = usersDao.findOne(id);
+        updateForm.setLogin(user.getLogin());
+        updateForm.setFirstName(user.getFirstName());
+        updateForm.setSecondName(user.getSecondName());
+        updateForm.setEmail(user.getEmail());
+        updateForm.setDate_created(user.getDateCreated());
+        updateForm.setIsAdmin(user.getIsAdmin());
+        modelAndView.addObject("update_form", updateForm);
+        return modelAndView;
 
     }
 
@@ -77,14 +75,15 @@ public class UsersController {
     public String update(@Valid @ModelAttribute("update_form") UsersUpdateForm form, BindingResult result) {
         LOGGER.debug("Received request for UPDATE data in table CATEGORY");
         if (result.hasErrors()) {
-            return "users_update";
+            return "user_update";
         } else {
-
-            User user = (User) usersDao.findOne(form.getId());
-/*
-            users.setName(form.getName());
-            users.setParentId(form.getParentId());
-*/
+            User user = usersDao.findOne(form.getId());
+            user.setLogin(form.getLogin());
+            user.setFirstName(form.getFirstName());
+            user.setSecondName(form.getSecondName());
+            user.setEmail(form.getEmail());
+            user.setDateCreated(form.getDate_created());
+            user.setIsAdmin(form.getIsAdmin());
             usersDao.save(user);
             return REDIRECT_TO_INDEX;
         }
@@ -93,7 +92,7 @@ public class UsersController {
     @RequestMapping(value = "/delete", method = RequestMethod.POST)
     public String delete(@RequestParam int id) {
         LOGGER.info("Received request for DELETE new data in table CATEGORY");
-        User user = (User) usersDao.findOne(id);
+        User user = usersDao.findOne(id);
         if (user == null) {
             LOGGER.debug("Users not found");
             return REDIRECT_TO_INDEX;
@@ -101,21 +100,4 @@ public class UsersController {
         usersDao.delete(user);
         return REDIRECT_TO_INDEX;
     }
-
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login() {
-        return "login";
-    }
-
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String loginPost(@RequestParam String login, HttpSession session) {
-        if (usersDao.findByLoginIgnoringCase(login)!=null){
-            session.setAttribute("login", login);
-            return REDIRECT_TO_INDEX;
-        }
-        else{
-            return "redirect:/users/login";
-        }
-    }
-
 }
